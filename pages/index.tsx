c91a1490ea3e6e894794f02939adbb3d38eb257b
ZwingliCaleb <calebzwingli@gmail.com>
2024-05-31 03:50:01 +0300

    cleaning up

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,6 @@ const IndexPage = () => {
   return (
     <div className="container mx-auto">
       <header className="flex justify-center items-center py-10">
-        {/*<Image src={logo} alt="Logo" priority />*/}
       </header>
       <div className="flex justify-center">
         <button className="bg-blue-500 text-white px-6 py-3 rounded-md" onClick={handleGoToDashboard}>
